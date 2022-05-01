10a8b7ee846ac50813a3dd402999c24f1c6d51ae
Antonio Almeida de Melo <antonio@pop-os.localdomain>
2022-05-01 01:31:05 -0300

    criando evento when scroll para mostrar button voltar a home e reestruturando o cod

--- main.js
+++ main.js
@@ -18,19 +18,41 @@ for (const link of links) {
 
 // mudar o header da pagina quando der scroll
 
+
+function changeHeaderWhenScroll(argument) {
+
 const header = document.querySelector("#header")
 const navHeight =  header.offsetHeight
 
-window.addEventListener('scroll',()=>{
+
   if (window.scrollY >= navHeight){
   //scroll maior que a altura do header
     header.classList.add('scroll')
-  }else{
+  } else{
    //menor que a altura do header
     header.classList.remove('scroll')
   }
-})
 
+}
+
+
+// back to top button
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if(window.scrollY >= 1000){
+    backToTopButton.classList.add('show')
+  }else{
+    backToTopButton.classList.remove('show')
+  }
+
+}
+
+//calling the functions when scroll
+window.addEventListener('scroll',()=>{
+  changeHeaderWhenScroll()
+  backToTop()
+  })
 
 
 
@@ -46,9 +68,11 @@ const swiper = new Swiper('.swiper-container',{
   keyboard:true
 })
 
+
+
 // ScrollReveal
 
-const scrollReveal = scrollReveal({
+const scrollReveal = ScrollReveal({
   origin:'top',
   distance:'30px',
   duration:700,
@@ -60,7 +84,9 @@ scrollReveal.reveal(
  #about .image, #about .text,
  #services header, #services .card,
  #testimonials header, #testimonials .testimonials
- #contact .text, #contact .links
+ #contact .text, #contact .links,
+ footer .brand, footer .social
  `,
  {interval:100}
 ) 
+
